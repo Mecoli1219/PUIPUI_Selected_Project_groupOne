5444ccb52186713104746418c1b5bc93700b489c
Mecoli1219 <michaellai901026@gmail.com>
2021-06-01 17:04:56 +0800

    mask red

--- python/main.py
+++ python/main.py
@@ -19,15 +19,28 @@ def main():
     camera = PiCamera();
     rawCapture = PiRGBArray(camera)
 
-    
-    n = 1
+    if (sys.argv[1] == '0'):
+        n = 1
 
-    while n == 1:
-        n = 2
-        time.sleep(0.1)
-        camera.capture(rawCapture, format="bgr")
-        image = rawCapture.array
-        print(image)
+        while n == 1:
+            n = 2
+            time.sleep(0.1)
+            camera.capture(rawCapture, format="bgr")
+            image = rawCapture.array
+            print(image)
+
+
+    elif (sys.argv[1] == '1'):
+        img = cv.imread("./src/test.jpg")
+        img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        mask1 = cv.inRange(img_hsv,(0,50,20),(5,255,255))
+        mask2 = cv.inRange(img_hsv,(175,50,20), (180,255,255))
+        mask = cv.bitwise_or(mask1, mask2)
+        croped = cv.bitwise_and(img,img, mask = mask)
+        print(img.shape)
+        print(mask.shape)
+        cv.imshow("croped", croped)
+        cv.waitKey()
 
 
 
---
+++ python/make.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = np.zeros((512, 512, 3),  np.uint8)
+
+cv.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+cv.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
+cv.circle(img, (447, 64), 64, (0, 0, 255), -1)
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.putText(img, 'SwordArtOnline', (10, 500),
+           font, 2, (255, 255, 255), 2, cv.LINE_AA)
+cv.imwrite("./src/test.jpg", img)
