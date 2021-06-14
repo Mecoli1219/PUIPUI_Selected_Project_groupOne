6564baee369d36761a29a09c43b150fef09e326c
spongebobaa16 <83540155+spongebobaa16@users.noreply.github.com>
2021-06-14 23:14:26 +0800

    Add files via upload

--- final_project_0608new/final_project_0608new.ino
+++ final_project_0608new/final_project_0608new.ino
@@ -1,8 +1,6 @@
 #include <Wire.h>
 #include <Servo.h>
 #include "movecar.h"
-#include <String.h>
-#include <stdio.h>
 
 
 // L298N, 請按照自己車上的接線寫入腳位(左右不一定要跟註解寫的一樣)
@@ -13,22 +11,11 @@ int MotorR_I4 = 7;   //定義 I4 接腳（右）
 int MotorL_PWML = 6; //定義 ENA (PWM調速) 接腳
 int MotorR_PWMR = 5; //定義 ENB (PWM調速) 接腳
 int Servo_signal = 9;
-
-int result;
-char data[50];
-char stopcar_c[50];
-char moveforward_c[50];
-char movebackward_c[50];
-char turnleft_c[50];
-char turnright_c[50];
-char catch_c[50];
-char drop_c[50];
-strcpy(moveforward_c,"straight");
-strcpy(turnleft_c,"left");
-strcpy(turnright_c,"right");
-strcpy(catch_c,"catch");
-strcpy(drop_c,"drop");
-
+String straight_s = "straight";
+String left_s = "left";
+String right_s = "right";
+String catch_s = "catch";
+String drop_s = "drop";
 
 Servo myservo;
 
@@ -44,8 +31,6 @@ void setup() {
   pinMode(MotorR_PWMR, OUTPUT);
   myservo.attach(Servo_signal);
   myservo.write(60);
-
-  
 #ifdef DEBUG
   Serial.println("Start!");
 #endif
@@ -58,30 +43,24 @@ int _Tp = 180;
 void loop() {
 
     if(Serial.available() > 0) {
-      
-      String data_ser = Serial.readStringUntil('\n');
-      strcpy(data,data_ser);
+      String data = Serial.readStringUntil('\n');
       //Serial.print("You sent me: ");
       //Serial.println(data);
-
-      if(strcmp(data,moveforward_c)==0){
+      
+      if(data == straight_s){
         move_forward(200,200);              
       }
-      else if(strcmp(data,turnleft_c)==0){
+      else if(data == left_s){
         turn_left(200,200);
       }
-      else if(strcmp(data,turnright_c)==0){
+      else if(data == right_s){
         turn_right(200,200);
       }
-      else if(strcmp(data,catch_c)==0){
-        stop_car();
+      else if(data == catch_s){
         catchbox();
       }
-      else if(strcmp(data,drop_c)==0){
-        stop_car();
+      else if(data == drop_s){
         drop();
-        move_backward(200,200);
-        delay(500);
       }
     }
 /*
